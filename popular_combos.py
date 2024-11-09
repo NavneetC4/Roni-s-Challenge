d4871e44d6211f7128b51a94d8ae0708f555b1b4
NavneetC4 <3cnav3@gmail.com>
2024-11-09 15:15:10 -0600

    Sandwhich

--- popular_combos.py
+++ popular_combos.py
@@ -16,6 +16,7 @@ sandwhich  = (october.loc[october["Parent Menu Selection"]=="Grilled Cheese Sand
 def mac_che(mac):
     cheeses = (mac.loc[mac["Option Group Name"]=="Choose Your Cheese"])
     cheese_counts =  cheeses['Modifier'].value_counts()
+<<<<<<< Updated upstream
     cheese_counts.plot(kind='bar')
   
     meat = (mac.loc[mac["Option Group Name"]=="Choose Your Meats"])
@@ -23,6 +24,18 @@ def mac_che(mac):
     meat_counts.plot(kind='bar')
 
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+=======
+    #cheese_counts.plot(kind='bar')
+  
+    meat = (mac.loc[mac["Option Group Name"]=="Choose Your Meats"])
+    meat_counts =  meat['Modifier'].value_counts()
+    # meat_counts.plot(kind='bar')
+    
+    drizzle = (mac.loc[mac["Option Group Name"]=="Choose Your Drizzles"])
+    drizzle_counts =  drizzle['Modifier'].value_counts()
+
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 5))
+>>>>>>> Stashed changes
     cheese_counts.plot(kind='bar', ax=axes[0], color='skyblue')
     axes[0].set_title('Cheese Distribution in Mac and Cheese')
     axes[0].set_xlabel('Cheese Types')
@@ -33,14 +46,30 @@ def mac_che(mac):
     axes[1].set_xlabel('Meat Types')
     axes[1].set_ylabel('Count')
 
+<<<<<<< Updated upstream
+=======
+    drizzle_counts.plot(kind='bar', ax=axes[2], color='grey')
+    axes[2].set_title('Drizzle Distribution in Mac and Cheese')
+    axes[2].set_xlabel('Drizzle Types')
+    axes[2].set_ylabel('Count')
+
+>>>>>>> Stashed changes
     # Annotate the bars with the counts
     for i in axes[0].containers:
         axes[0].bar_label(i)
     for i in axes[1].containers:
         axes[1].bar_label(i)
+<<<<<<< Updated upstream
 
     plt.tight_layout()
     plt.show()s
+=======
+    for i in axes[2].containers:
+        axes[2].bar_label(i)
+
+    plt.tight_layout()
+    plt.show()
+>>>>>>> Stashed changes
 
     
 mac_che(mac_cheese)
