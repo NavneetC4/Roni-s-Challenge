ab0012d351d3fb49e0563fb3074f968c31743648
Kushal <kushal.R.B@outlook.com>
2024-11-10 09:46:50 -0600

    Finds the most common combo ordered

--- comboFinder.py
+++ comboFinder.py
@@ -1,3 +1,53 @@
 import dataFiles
 import pandas as pd
 
+
+listOfMonths = [dataFiles.aprilCSV,dataFiles.mayCSV,dataFiles.juneCSV,dataFiles.julyCSV,dataFiles.augustCSV,dataFiles.septemberCSV,dataFiles.octoberCSV]
+
+dict = {
+    "userID": [],
+    "userModifiers":[]
+}
+
+#Define outside here so its not being aletered
+extractUserID = ""
+
+for dataframes in listOfMonths:
+    extractMonth = id(dataframes)
+    for index,rows in dataframes.iterrows():
+        try:
+            extractUserID = rows["Order ID"] +"_" + f"{extractMonth}"
+            
+            # Use the user id as the key, then the value is a dictionary that stores the a string with the user's modifiers
+            # the second value is the count
+            
+            if(not (extractUserID in dict["userID"]) ):
+                #intialize the dictionary that stores what a user has ordered
+                dict["userID"] += [extractUserID]
+                dict["userModifiers"]+=[rows["Modifier"]]
+            else:
+                dict["userModifiers"][dict["userID"].index(extractUserID)] += ","+rows["Modifier"]
+        except:
+            continue
+    
+
+dictToDf = pd.DataFrame(dict)
+
+countDf = dictToDf["userModifiers"].value_counts()
+print(countDf)
+
+#Get only those elemetns with commas in them to find the top combos
+comboDF = countDf[countDf.index.str.contains(",")].iloc[0:10]
+
+
+
+
+
+
+
+
+
+
+
+
+
